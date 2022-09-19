6eaafa1bf2a18236cf38cf4fdb1ebe0c80fd5258
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-09-19 18:38:01 +0000

    ベースを記述

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", \
-    title = 'index.html')
+    return render_template("index.html")
 
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
+@app.route("/confirm")
+def confirm():
+    return render_template("confirm.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
