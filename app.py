372a81034dee7aa4b18e1c9635d91206a6ed92f7
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-09-23 22:03:10 +0900

    result画面の処理を追加

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
+import spreadsheet
 
 app = Flask(__name__)
 
@@ -14,6 +16,14 @@ def order():
 def confirm():
     return render_template("confirm.html")
 
-@app.route("/result")
+@app.route("/result", methods=["POST"])
 def result():
-    return render_template("result.html")
+    
+    cheese = request.form["cheese"]
+    butter = request.form["butter"]
+    salt = request.form["salt"]
+    ketchup = request.form["ketchup"]
+    item = [cheese,butter,salt,ketchup]
+    
+    order_id = spreadsheet.main(item)
+    return render_template("result.html", order_id = order_id)
