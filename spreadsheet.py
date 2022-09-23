b8574093601d799f19ac53c4dc10bb56fa98de20
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-09-24 00:37:49 +0900

    インデントと修正

--- spreadsheet.py
+++ spreadsheet.py
@@ -23,33 +23,32 @@ def get_time():
   return now.time().strftime('%X')
 
 def next_available_row(worksheet):
-    str_list = list(filter(None, worksheet.col_values(2)))
-    return str(len(str_list)+1)
+  str_list = list(filter(None, worksheet.col_values(2)))
+  return str(len(str_list)+1)
 
 def main(order_item):
-    # ランダム生成されるID
-    ORDER_ID = random.randint(100,999)
-    CREDENTIAL = os.environ['CREDENTIAL_JSON']
-    SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
+  # ランダム生成されるID
+  ORDER_ID = random.randint(100,999)
+  SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
 
-    scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(CREDENTIAL, scope)
+  scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_dict(CREDENTIAL_JSON, scope)
 
-    # 認証情報を使ってスプレッドシートの操作権を取得
-    gs = gspread.authorize(credentials)
-    worksheet = gs.open_by_key(SPREADSHEET_ID).sheet1
+  # 認証情報を使ってスプレッドシートの操作権を取得
+  gs = gspread.authorize(credentials)
+  worksheet = gs.open_by_key(SPREADSHEET_ID).sheet1
 
-    # 書き込み可能な行を取得
-    next_row = next_available_row(worksheet)
+  # 書き込み可能な行を取得
+  next_row = next_available_row(worksheet)
 
-    # IDを書き込む
-    worksheet.update_cell(next_row,1,ORDER_ID)
-    # 個数を書き込む
-    worksheet.append_row(order_item,table_range='B'+ next_row)
-    # 時間を書き込む
-    worksheet.update_cell(next_row,8,get_time())
+  # IDを書き込む
+  worksheet.update_cell(next_row,1,ORDER_ID)
+  # 個数を書き込む
+  worksheet.append_row(order_item,table_range='B'+ next_row)
+  # 時間を書き込む
+  worksheet.update_cell(next_row,8,get_time())
 
-    # 降順にソート
-    worksheet.sort((8,'des'))
+  # 降順にソート
+  worksheet.sort((8,'des'))
 
-    return ORDER_ID
+  return ORDER_ID
