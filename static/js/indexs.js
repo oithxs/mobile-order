f9894e658470866d7c5e25dca1e2e245633d042f
sho037 <103867061+sho037@users.noreply.github.com>
2022-10-08 01:01:03 +0000

    フォーマットを実行

--- static/js/indexs.js
+++ static/js/indexs.js
@@ -7,23 +7,23 @@ var myDate = String(myD.getDate());
 
 //開発時のみ下記の比較演算子は==で本番は!=に変更してください
 if (loading_data != myYear + myMonth + myDate) {
-    $("#loading").css("display", "block");
-    setTimeout(function () {
-        $("#loading_logo").fadeIn(1000, function () {
-            setTimeout(function () {
-                $("#loading_logo").fadeOut(1000);
-            }, 1000);
-            setTimeout(function () {
-                $("#loading").fadeOut(1000, function () {
-                    var myD = new Date();
-                    var myYear = String(myD.getFullYear());
-                    var myMonth = String(myD.getMonth() + 1);
-                    var myDate = String(myD.getDate());
-                    $.cookie("accessdate", myYear + myMonth + myDate);
-                });
-            }, 1700);
+  $("#loading").css("display", "block");
+  setTimeout(function () {
+    $("#loading_logo").fadeIn(1000, function () {
+      setTimeout(function () {
+        $("#loading_logo").fadeOut(1000);
+      }, 1000);
+      setTimeout(function () {
+        $("#loading").fadeOut(1000, function () {
+          var myD = new Date();
+          var myYear = String(myD.getFullYear());
+          var myMonth = String(myD.getMonth() + 1);
+          var myDate = String(myD.getDate());
+          $.cookie("accessdate", myYear + myMonth + myDate);
         });
-    }, 1000);
+      }, 1700);
+    });
+  }, 1000);
 } else {
-    $("#loading").css("display", "none"); //同日2回目のアクセスでローディング画面非表示
+  $("#loading").css("display", "none"); //同日2回目のアクセスでローディング画面非表示
 }
