976a0207ac8becce39eeaa8f4a594fc59832ea5d
sho037 <103867061+sho037@users.noreply.github.com>
2022-10-08 00:57:51 +0000

    フォーマットを実行

--- static/js/orders.js
+++ static/js/orders.js
@@ -1,13 +1,12 @@
 (function () {
   // polyfill
-  if (!Element.prototype.matches) 
+  if (!Element.prototype.matches)
     Element.prototype.matches = Element.prototype.msMatchesSelector;
   if (!Element.prototype.closest) {
     Element.prototype.closest = function (s) {
       var el = this;
       do {
-        if (el.matches(s)) 
-          return el;
+        if (el.matches(s)) return el;
         el = el.parentElement || el.parentNode;
       } while (el !== null && el.nodeType === 1);
       return null;
@@ -21,35 +20,21 @@
     let value = parseFloat(input.value);
     const min = parseFloat(input.min);
     const max = parseFloat(input.max);
-    const step = !isNaN(parseFloat(input.step))
-      ? parseFloat(input.step)
-      : 1;
+    const step = !isNaN(parseFloat(input.step)) ? parseFloat(input.step) : 1;
 
-    value = (
-      isNaN(value)
-      ? 0
-      : value) + (
-      increment
-      ? step
-      : -step);
+    value = (isNaN(value) ? 0 : value) + (increment ? step : -step);
 
-    if (!isNaN(min) && value < min) 
-      input.value = min;
-    else if (!isNaN(max) && value > max) 
-      input.value = max;
-    else 
-      input.value = value;
-    }
-  ;
+    if (!isNaN(min) && value < min) input.value = min;
+    else if (!isNaN(max) && value > max) input.value = max;
+    else input.value = value;
+  };
   // クリック時
   const click = function (t) {
     if (t.matches(".spin .spin-minus, .spin .spin-plus")) {
-      if (intervalId != null) 
-        return;
+      if (intervalId != null) return;
       const input = t.closest(".spin").querySelector(".spin-input");
-      if (!input) 
-        return;
-      
+      if (!input) return;
+
       valueUpdate(input, t.matches(".spin-plus"));
     }
   };
@@ -57,15 +42,13 @@
   const down = function (t) {
     if (t.matches(".spin .spin-minus, .spin .spin-plus")) {
       const input = t.closest(".spin").querySelector(".spin-input");
-      if (!input) 
-        return;
+      if (!input) return;
       const increment = t.matches(".spin-plus");
 
       let delay = 500;
       let start = new Date().getTime();
       intervalId = setInterval(function () {
-        if (new Date().getTime() - start <= delay) 
-          return; // 開始の間隔をずらす (clickとの兼ね合い)
+        if (new Date().getTime() - start <= delay) return; // 開始の間隔をずらす (clickとの兼ね合い)
         valueUpdate(input, increment);
       }, 50);
     }
@@ -99,7 +82,7 @@
 
 // input[type=number] 入力調整
 (function () {
-  if (!Element.prototype.matches) 
+  if (!Element.prototype.matches)
     Element.prototype.matches = Element.prototype.msMatchesSelector;
   document.addEventListener("input", function (e) {
     if (e.target.matches("input[type=number]")) {
@@ -108,10 +91,8 @@
       const min = parseFloat(input.min);
       const max = parseFloat(input.max);
 
-      if (!isNaN(min) && value < min) 
-        input.value = min;
-      else if (!isNaN(max) && value > max) 
-        input.value = max;
-      }
-    });
+      if (!isNaN(min) && value < min) input.value = min;
+      else if (!isNaN(max) && value > max) input.value = max;
+    }
+  });
 })();
