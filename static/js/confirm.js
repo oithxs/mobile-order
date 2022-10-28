8587be1e1a8d303ce3ecdb5105694e9b621058b9
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-10-28 13:35:46 +0900

    スピナーの追加

--- static/js/confirm.js
+++ static/js/confirm.js
@@ -4,6 +4,8 @@ $(function () {
 
     $("#cancel-btn").css("pointer-events", "none").attr("tabindex", -1); //aタグ(キャンセルボタン)を非活性化
 
+    $("#loading").removeClass("d-none"); // スピナーを表示させる
+
     $(this).closest("form").submit(); //フォームを送信する
   });
 });
