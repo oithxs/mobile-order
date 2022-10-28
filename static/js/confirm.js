7cdd72c893b47c2055f8af517035d038037faf8c
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-10-28 13:14:05 +0900

    キャンセルボタンの非活性化

--- static/js/confirm.js
+++ static/js/confirm.js
@@ -1,6 +1,9 @@
 $(function () {
   $('[type="submit"]').click(function () {
     $(this).prop("disabled", true); //ボタンを無効化する
+
+    $("#cancel-btn").css("pointer-events", "none").attr("tabindex", -1); //aタグ(キャンセルボタン)を非活性化
+
     $(this).closest("form").submit(); //フォームを送信する
   });
 });
